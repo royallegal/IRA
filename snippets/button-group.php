71c4c4959dd37589d566f32f4ca92729f43d4d62
Mark Swedberg <swedy13@gmail.com>
2018-03-13 20:21:50 -0500

    Default Hero Update

--- snippets/button-group.php
+++ snippets/button-group.php
@@ -3,56 +3,46 @@
     if (have_rows("buttons")) {
         while (have_rows("buttons")) {
             the_row();
-            $randId = substr(md5(microtime()),rand(0,26),10); 
-            /* $style   = get_sub_field("style");*/
-            $type    = get_sub_field("type");
+            $randId = substr(md5(microtime()),rand(0,26),10);
+
+            // Basic Info
+            $title   = get_sub_field("link")["title"];
+            $label   = get_sub_field('button_label');
+            $text    = ($label) ? $label : $title;
+            $icon    = get_sub_field('icon');
+            $iconPos = get_sub_field('icon_position');
+
+            // Behaviour
+            $url     = get_sub_field("link")["url"];
+            $modal   = get_sub_field('modal');
+            $target  = get_sub_field("link")["target"];
+            $video   = get_sub_field('video');
+            $video   = preg_replace('/src="(.+?)"/', 'src="$1&enablejsapi=1"', $video);
+
+            $href = ($modal) ? "#videoModal".$randId : $url;
+
+            // Styling
+            $styles  = implode(" ", get_sub_field("styles"));
             $size    = get_sub_field("size");
             $color   = get_sub_field("color");
             $variant = get_sub_field("color_variant");
             $hover   = get_sub_field("hover");
-            $modal   = get_sub_field('modal');
-            $button  = get_sub_field('button_label');
-            $icon    = get_sub_field('icon');
-            $iconPos = get_sub_field('icon_position');
-            
-            $classes = $type.' '.$size.' '.$color.' '.$variant.' '.$hover;
-            
-            $url     = get_sub_field("link")["url"];
-            $title   = get_sub_field("link")["title"];
-            $target   = get_sub_field("link")["target"];
-            
-            $video   = get_sub_field('video');
-            $video = preg_replace('/src="(.+?)"/', 'src="$1&enablejsapi=1"', $video);
-        ?>
-        <?php  if( $modal && in_array('Modal', $modal) ): ?> 
-            <a href="#videoModal<?=$randId?>" class="<?= $classes; ?> button modal-trigger">
-                <?php if( $iconPos == 'left' ): ?>
-                    <i class="material-icons mr-1"><?= $icon; ?></i>
-                <?php endif; ?>
-                    <?= $button; ?>    
-                <?php if( $iconPos == 'right' ): ?>
-                    <i class="material-icons ml-1"><?= $icon; ?></i>
-                <?php endif; ?>
-            </a>
+            $popup   = $modal ? 'modal-trigger' : '';
+            $classes = $styles.' '.$size.' '.$color.' '.$variant.' '.$hover.' '.$popup;
+    ?>
+
+        <a href="<?= $href; ?>" class="<?= $classes; ?> button" target="<?= $target; ?>">
+            <i class="material-icons <?= $iconPos; ?>"><?= $icon; ?></i>
+            <?php echo $text ?>
+        </a>
+
+        <?php if ($modal) : ?>
             <div id="videoModal<?=$randId?>" hero-video-modal class="modal video">
                 <div class="video-container">
                     <?= $video ?>
                 </div>
             </div>
-        <?php else: ?>
-            <a href="<?= $url; ?>" class="<?= $classes; ?> button" target="<?= $target; ?>">
-                <?php if( $iconPos == 'left' ): ?>
-                    <i class="material-icons mr-1"><?= $icon; ?></i>
-                <?php endif; ?>
-                    <?= $title; ?>
-                <?php if( $iconPos == 'right' ): ?>
-                    <i class="material-icons ml-1"><?= $icon; ?></i>
-                <?php endif; ?>
-            </a>
         <?php endif; ?>
 
-    <?php
-    }
-    }
-    ?>
+    <?php } } ?>
 </div>
--- snippets/title-group.php
+++ snippets/title-group.php
@@ -2,20 +2,20 @@
 $title        = get_sub_field('title');
 $subtitle     = get_sub_field('subtitle');
 $description  = get_sub_field('description');
-if ($image || $background == "dark") {
+if ($image || $background !== "light") {
     $text_classes = "white-text t-depth-1-half";
 }
 ?>
 
 <div class="<?php echo $text_classes; ?> title-group">
     <?php if ($title) { ?>
-        <p class="h1 title">
+        <p class="title">
             <?php echo $title ?>
         </p>
     <?php } ?>
 
     <?php if ($subtitle) { ?>
-        <p class="h2 subtitle">
+        <p class="subtitle">
             <?php echo $subtitle ?>
         </p>
     <?php } ?>
